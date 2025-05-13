e939bb76e275d014315c637412946604124cfef1
4ouChan <kumho3983398@naver.com>
2025-05-13 17:49:36 +0900

    데이터베이스 생성

---
+++ schedule.sql
@@ -0,0 +1,16 @@
+use schedule;
+
+CREATE TABLE user (
+    userId BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '사용자 식별자',
+    userName VARCHAR(50) COMMENT '사용자 이름',
+    password VARCHAR(50) COMMENT '비밀번호'
+);
+
+CREATE TABLE schedule (
+    scheduleId BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '일정 식별자',
+    userId BIGINT NOT NULL ,
+    schedule VARCHAR(2500) COMMENT '할 일',
+    createDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시간',
+    updateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '수정 시간',
+    FOREIGN KEY (userId) REFERENCES user(userId)
+);
